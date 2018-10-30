3af4510e7b172747b0222b19084b7250f269965f
Fabio Oliveira (FG-233) <fabio.oliveira@bmw.de>
2018-10-30 13:12:13 +0100

    Add cache configuration

--- src/main/java/com/bmwgroup/weatherservice/application/WeatherService.java
+++ src/main/java/com/bmwgroup/weatherservice/application/WeatherService.java
@@ -1,6 +1,7 @@
 package com.bmwgroup.weatherservice.application;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,6 +12,8 @@ public class WeatherService {
     @Autowired
     private WeatherProviderClient weatherProviderClient;
 
+
+    @Cacheable(cacheNames = "currentWeatherPerCity", key="#cityName.get()")
     public WeatherData current(Optional<String> cityName) {
         WeatherProviderForecast forecast = weatherProviderClient.current(cityName);
         return new WeatherData(forecast.getTemperature(), forecast.getDescription());

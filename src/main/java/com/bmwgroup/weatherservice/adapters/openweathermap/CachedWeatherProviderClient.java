cf261e01c36b210274f4485834d72d25d64b0720
Fabio Oliveira (FG-233) <fabio.oliveira@bmw.de>
2018-10-31 18:34:20 +0100

    Add logging info

--- src/main/java/com/bmwgroup/weatherservice/adapters/openweathermap/CachedWeatherProviderClient.java
+++ src/main/java/com/bmwgroup/weatherservice/adapters/openweathermap/CachedWeatherProviderClient.java
@@ -2,6 +2,8 @@ package com.bmwgroup.weatherservice.adapters.openweathermap;
 
 import com.bmwgroup.weatherservice.application.WeatherProviderClient;
 import com.bmwgroup.weatherservice.application.WeatherProviderForecast;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.Cacheable;
@@ -18,6 +20,8 @@ public class CachedWeatherProviderClient implements WeatherProviderClient {
 
     private WeatherProviderClient decorated;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CachedWeatherProviderClient.class);
+
     public CachedWeatherProviderClient(@Autowired @Qualifier( "openweathermap") WeatherProviderClient decorated) {
         this.decorated = decorated;
     }
@@ -25,6 +29,8 @@ public class CachedWeatherProviderClient implements WeatherProviderClient {
     @Override
     @Cacheable(cacheNames = "currentWeatherPerCity", key="#cityName.get()")
     public WeatherProviderForecast current(Optional<String> cityName) {
+        LOGGER.debug("Not using cached version, calling the decorated client instead");
+
         return decorated.current(cityName);
     }
 }
